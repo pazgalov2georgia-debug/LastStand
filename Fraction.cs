a589b6f289e42d94842a1cda4f0dab2ae6c29070
Pazgalov2georgia@gmail.com <Pazgalov@DESKTOP-V51K7QM>
2026-01-25 22:37:53 +0400

    puk

---
+++ Fraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastStand
+{
+    internal class Fraction
+    {
+        public string Name;
+        public List<Squad> Formations;
+
+        public Fraction (string name, List<Squad> formations)
+        {
+        Name = name;
+        Formations = formations;
+        }
+    }
+}
---
+++ TheBattle.cs
@@ -0,0 +1,53 @@
+﻿using Battle;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastStand
+{
+    internal class TheBattle
+    {
+        public TheBattle()
+        {
+            string[] fightersNames = File.ReadAllLines("FightersNames.txt");
+            var rightSquadsNames = new List<string> { "Alfa", "Beta" };
+            var wrongSquadsNames = new List<string> { "Alfa", "Beta" };
+            var right = CreateFraction("Right", rightSquadsNames, fightersNames);
+            var wrong = CreateFraction("Wrong", wrongSquadsNames, fightersNames);
+        }
+        private Fraction CreateFraction(string name, List <string>squadsNames, string[] fightersNames)
+        {
+            
+            List<Squad> squads = new List<Squad>();
+            foreach (var squadName in squadsNames)
+            {
+                var squad = CreateSquad(squadName, fightersNames);
+                squads.Add(squad);
+            }
+            return new Fraction(name, squads);
+        }
+        private Squad CreateSquad(string name, string[] fightersNames)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+               var fighther = CreateFighter(name, fightersNames);
+            }
+            return new Squad(name, fighter);
+        }
+        private Fighter CreateFighter(string name, string[] fightersNames)
+        {
+            Random rand = new Random();
+            List<Fighter> fighters = new List<Fighter>();
+            {
+                
+                var fighterName = fightersNames[rand.Next(fightersNames.Length - 1)];
+                var fighter = new Fighter(fighterName);
+            }
+            return Fighter.Create(fighter);
+        }
+    }
+}
