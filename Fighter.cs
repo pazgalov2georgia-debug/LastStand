d76913816b63b56b39b7b06fcba668d05f360190
Pazgalov2georgia@gmail.com <Pazgalov@DESKTOP-V51K7QM>
2026-01-25 18:57:30 +0400

    Add project files.

---
+++ Fighter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle
+{
+    public class Fighter
+    {
+        public string Name;
+        public int HealthPool;
+        public int Attack;
+        public int Strenght;
+        public Fighter()
+        {
+
+        }
+    }
+}
